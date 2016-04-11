c81055bed693e61d427c5ecc4db96551b7f2aae2
rmhowe <tiaeph@gmail.com>
2016-04-11 12:50:48 +0100

    Adds info windows to the map

--- src/js/components/ArtistMap.js
+++ src/js/components/ArtistMap.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { GoogleMapLoader, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import Immutable from 'immutable';
 
 export default class ArtistMap extends React.Component {
   static propTypes = {
     artists: React.PropTypes.object.isRequired
   };
 
+  state = {
+    openInfoWindows: Immutable.Map()
+  };
+
+  handleMarkerClick(artist) {
+    this.setState({
+      openInfoWindows: this.state.openInfoWindows.set(artist.get('uuid'), true)
+    });
+  }
+
+  handleInfoWindowClose(artist) {
+    this.setState({
+      openInfoWindows: this.state.openInfoWindows.set(artist.get('uuid'), false)
+    });
+  }
+
   getArtistMarkers() {
-    return this.props.artists.map((artist) => {
+    return this.props.artists.map((artist, i) => {
+      const infoWindow = this.getInfoWindow(artist);
       return (
         <Marker
           key={artist.get('uuid')}
@@ -15,9 +33,26 @@ export default class ArtistMap extends React.Component {
             lat: parseFloat(artist.get('latitude')),
             lng: parseFloat(artist.get('longitude'))
           }}
-        />
+          onClick={this.handleMarkerClick.bind(this, artist)}
+        >
+          {infoWindow}
+        </Marker>
       );
     });
+  }
+
+  getInfoWindow(artist) {
+    if (this.state.openInfoWindows.get(artist.get('uuid'))) {
+      return (
+        <InfoWindow onCloseclick={this.handleInfoWindowClose.bind(this, artist)}>
+          <div>
+            <strong>Age</strong> {artist.get('age')}<br/>
+            <strong>Gender</strong> {artist.get('gender')}<br/>
+            <strong>Rate</strong> {artist.get('rate')}
+          </div>
+        </InfoWindow>
+      );
+    }
   }
 
   render() {
